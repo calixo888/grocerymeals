1421e72bb6a0b5e79f7fec2221d700c501c0d9a3
Calix Huang <calix.huang1@gmail.com>
2020-01-14 18:56:28 -0800

    added security enviroment variables

--- grocerymeals_project/settings.py
+++ grocerymeals_project/settings.py
@@ -135,8 +135,8 @@ if development:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     AWS_DEFAULT_ACL = None
-    AWS_ACCESS_KEY_ID = 'AKIAT2SKEKLO7J5HFM6W'
-    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = 'grocerymeals-assets'
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {
